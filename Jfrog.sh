78233f3f30ceda12ff8cfbd0a67527a72bf8df68
svvprasad75 <svvprasad75@gmail.com>
2024-10-04 15:03:37 +0530

    Update Jfrog.sh

--- Jfrog.sh
+++ Jfrog.sh
@@ -9,7 +9,8 @@ cd $JFROG_HOME/artifactory/var/etc/
 touch ./system.yaml
 chown -R 1030:1030 $JFROG_HOME/artifactory/var
 chmod -R 777 $JFROG_HOME/artifactory/var
-Run the below image for JFROG
+
+#Run the below image for JFROG
 docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-oss:7.77.5
 UI will get open on PORT 8082/ui
 http://IPADDRESS OF YOUR SERVER:8082/ui
